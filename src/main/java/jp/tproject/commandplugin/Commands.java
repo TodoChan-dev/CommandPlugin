ed2df340cc88cfb7ad7d1ce8bf96c5450c551895
TodoChan-dev <todomenn@gmail.com>
2023-12-26 11:50:23 +0900

    最初のcommitやで

---
+++ src/main/java/jp/tproject/commandplugin/Commands.java
@@ -0,0 +1,12 @@
+package jp.tproject.commandplugin;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Command implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings) {
+        return false;
+    }
+}
